54fb424498eb57ffc74452c008edc7cc79318873
Jan Niklas Engel <captndata@gmail.com>
2021-10-12 22:22:11 +0200

    Improve type annotations in task

--- task.py
+++ task.py
@@ -1,42 +1,39 @@
 import copy
 from dataclasses import dataclass, field
 from typing import List
+from __future__ import annotations
 
 
-@dataclass()
+@dataclass
 class Task:
     id: int
     processing_time: int
-    predecessors: List[int] = field(default_factory=list, init=False, repr=False)
-    setup_times: List[int] = field(default_factory=list, init=False, repr=False)
-
-    def __eq__(self, other) -> bool:
-        """A task is identified by its id"""
-        return self.id == other.id
+    predecessors: List[int] = field(default_factory=list, compare=False, init=False, repr=False)
+    setup_times: List[int] = field(default_factory=list, compare=False, init=False, repr=False)
 
     def has_predecessors(self) -> bool:
         """Returns True if the list of predecessors is not empty"""
         return len(self.predecessors)
     
-    def has_predecessor(self, task) -> bool:
-        """Return True if task is a predecessor of self"""
-        return task.id in self.predecessors
+    def has_predecessor(self, other: Task) -> bool:
+        """Returns True if other is a predecessor of self"""
+        return other.id in self.predecessors
     
-    def remove_predecessor(self, task) -> None:
+    def remove_predecessor(self, other: Task) -> None:
         """Remove a task from the list of predecessors"""
-        self.predecessors.remove(task.id)
+        self.predecessors.remove(other.id)
     
-    def setup_time(self, task) -> int:
+    def setup_time(self, other: Task) -> int:
         """Returns the setup time from self to the task passed as an argument"""
-        return self.setup_times[task.id]
+        return self.setup_times[other.id]
 
-    def __deepcopy__(self, memo):
+    def __deepcopy__(self, memo) -> Task:
 
         id_self = id(self)
 
         _copy = memo.get(id_self)
         if _copy is None:
-            _copy = type(self)(
+            _copy = Task(
                 copy.deepcopy(self.id, memo), copy.deepcopy(self.processing_time, memo)
             )
             _copy.predecessors = self.predecessors.copy()
