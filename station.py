f5f4ac8c6d06c3347a05d3f3beaf60e9873c4157
Jan Niklas Engel <captndata@gmail.com>
2021-10-21 22:36:01 +0200

    Make Station a Sequence

--- station.py
+++ station.py
@@ -1,37 +1,34 @@
+from __future__ import annotations
 from dataclasses import dataclass, field
-from typing import List
+from collections.abc import Sequence
+from typing import Counter, List
 
 from task import Task
 
 
 @dataclass(eq=False)
-class Station:
+class Station(Sequence):
     task_list: List[Task] = field(default_factory=list)
+
+    def __getitem__(self, i: int) -> Task:
+        """Return the task at index of the Stations task list"""
+        return self.task_list[i]
+    
+    def __len__(self) -> int:
+        return len(self.task_list)
     
     def add(self, task: Task) -> None:
         """Adds a task to the Station"""
         self.task_list.append(task)
         
-    def remove(self, task: Task) -> None:
-        """Removes a task from the Station"""
-        self.task_list.remove(task)
-        
     def empty(self) -> bool:
         """Returns True if there are no tasks assigned to the Station"""
-        return len(self.task_list) == 0
-    
-    def first(self) -> Task:
-        """Returns the first task in the Station"""
-        return self.task_list[0]
-    
-    def last(self) -> Task:
-        """Returns the last task in the Station"""
-        return self.task_list[-1]
+        return len(self) == 0
     
     def predecessor(self, other: Task) -> Task:
         """Return the predecessor of task in this station"""
-        task_index = self.task_list.index(other)
-        return self.task_list[task_index - 1]
+        task_index = self.index(other)
+        return self[task_index - 1]
     
     def fits_task(self, other: Task, cycle_time: int) -> bool:
         """Returns True if adding the task to the station does not exceed the cycle time
@@ -40,7 +37,7 @@ class Station:
         
         self.add(other)
         station_time = self.get_time()
-        self.remove(other)
+        self.task_list.remove(other)
         
         return station_time <= cycle_time
         
@@ -50,11 +47,10 @@ class Station:
         if self.empty():
             # An empty station has 0 station time
             return 0
-        elif len(self.task_list) == 1:
+        elif len(self) == 1:
             # A station with a single task only needs to process this task
-            return self.first().processing_time
+            return self[0].processing_time
         else:
             # If a station has more than two tasks, setup times need to be considered
-            per_task_values = [self.predecessor(task).setup_time(task) + task.processing_time for task in self.task_list]
+            per_task_values = [self.predecessor(task).setup_time(task) + task.processing_time for task in self]
             return sum(per_task_values)
-        
